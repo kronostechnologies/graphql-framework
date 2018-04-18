3b3444824d27909e618a989a5db2307e2a530fa2
Marc-Andre Boily <maboily@kronostechnologies.com>
2018-04-18 16:58:32 -0400

    Start work on Resolver

--- src/Kronos/GraphQLFramework/Resolver/Resolver.php
+++ src/Kronos/GraphQLFramework/Resolver/Resolver.php
@@ -4,10 +4,22 @@
 namespace Kronos\GraphQLFramework\Resolver;
 
 
+use Kronos\GraphQLFramework\FrameworkConfiguration;
+use Kronos\GraphQLFramework\Resolver\Context\ContextUpdater;
+
 class Resolver
 {
 	/**
-	 * @var GraphQLContext
+	 * @var ContextUpdater
 	 */
-	protected $activeContext;
+	protected $contextUpdater;
+
+	/**
+	 * @param FrameworkConfiguration $configuration
+	 */
+	public function __construct(FrameworkConfiguration $configuration)
+	{
+		$this->contextUpdater = new ContextUpdater();
+		$this->contextUpdater->setConfiguration($configuration);
+	}
 }
