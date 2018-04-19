0ffadecdcb67b8ebedc2d0fb6e747b1083ee8c83
Marc-Andre Boily <maboily@kronostechnologies.com>
2018-04-19 10:42:02 -0400

    Finished BaseController

--- src/Kronos/GraphQLFramework/BaseController.php
+++ src/Kronos/GraphQLFramework/BaseController.php
@@ -4,8 +4,23 @@
 namespace Kronos\GraphQLFramework;
 
 
+use Kronos\GraphQLFramework\Resolver\Context\GraphQLContext;
+
 class BaseController
 {
+	/**
+	 * @var GraphQLContext
+	 */
+	protected $context;
+
+	/**
+	 * @param GraphQLContext $context
+	 */
+	public function __construct(GraphQLContext $context)
+	{
+		$this->context = $context;
+	}
+
 	/**
 	 * Returns the name of the function that should be executed for resolving a field. It does not check if the
 	 * function exists.
