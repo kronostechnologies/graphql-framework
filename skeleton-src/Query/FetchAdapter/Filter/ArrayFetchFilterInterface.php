daebf97345cbcdeefdfe8a2ccd803a569f38f3c5
Marc-Andre Boily <maboily@kronostechnologies.com>
2018-05-25 13:32:27 -0400

    Remove skeleton-src folder

--- skeleton-src/Query/FetchAdapter/Filter/ArrayFetchFilterInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Defines a filter which can be applied on an ArrayFetchAdapter.
- */
-interface ArrayFetchFilterInterface extends FilterInterface
-{
-    public function filterArrayResults(array $value);
-}
